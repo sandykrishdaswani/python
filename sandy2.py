53ac4418343987343213d639aa7763088e814feb
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-06-21 14:29:21 +0530

    Update sandy2.py

--- sandy2.py
+++ sandy2.py
@@ -1,6 +1,8 @@
 num=int(input("Enter a number"))
-if(num%2)==0:
-  print("{0} is Even".format(num))
+if(num<0):
+  print("invalid")
+elif(num%2)==0:
+  print("Even")
 else:
-  print("{0} is Odd". format(num))
+  print("Odd")
   
